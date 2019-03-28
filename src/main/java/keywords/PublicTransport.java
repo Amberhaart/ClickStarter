8e7ce6404cde9286ea1463bf00428dc5a83db3c4
Unknown <sarkye@hotmail.com>
2019-03-28 16:33:18 +0100

    init(): new methods, not working yet

--- src/main/java/keywords/PublicTransport.java
+++ src/main/java/keywords/PublicTransport.java
@@ -2,6 +2,7 @@ package keywords;
 
 import configuration.Config;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,7 +13,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PublicTransport {
-  private static String home = "Móricz Zsigmond körtér";
+  private static String home = "Bertalan Lajos u., Budapest, 1111";
   private static String workplace = "Andrássy út 66.";
 
 
@@ -22,28 +23,49 @@ public class PublicTransport {
     driver.manage().deleteAllCookies();
     driver.manage().window().maximize();
     driver.get("https://www.google.com/maps/dir///@47.5115653,19.0587185,15z/data=!4m2!4m1!3e3");
-    getPublicRoute(driver);
+    WebDriverWait wait = new WebDriverWait(driver, Config.timeOutInSeconds);
+    String firstType = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/jsl/div[3]/div[7]/div[9]/div/div[1]/div/div/div[3]/div[2]/div/div[2]/span[1]/span[1]/img"))).getAttribute("alt");
+    String firstNumber = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/jsl/div[3]/div[7]/div[9]/div/div[1]/div/div/div[3]/div[2]/div/div[2]/span[1]/span[2]/span[1]/span[1]/span"))).getText();
+    setRoute(driver);
+    getPublicRoute(driver, firstType, firstNumber);
   }
-  public static void setRoute(WebDriver driver){
+
+  public static void setRoute(WebDriver driver) {
     WebDriverWait wait = new WebDriverWait(driver, Config.timeOutInSeconds);
     wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/jsl/div[3]/div[7]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div/div/input"))).sendKeys(home);
     wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/jsl/div[3]/div[7]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div/div/input"))).sendKeys(Keys.ENTER);
     wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/jsl/div[3]/div[7]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[2]/div[2]/div/div/input"))).sendKeys(workplace);
     wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/jsl/div[3]/div[7]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[2]/div[2]/div/div/input"))).sendKeys(Keys.ENTER);
+    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/jsl/div[3]/div[7]/div[9]/div/div[1]/div/div/div[5]/div[1]/div[2]/div[2]/div[4]/button"))).click();
   }
 
-  public static String getPublicRoute(WebDriver driver){
+  public static String getNumberAndType(WebDriver driver, String type, String number) {
     WebDriverWait wait = new WebDriverWait(driver, Config.timeOutInSeconds);
-    setRoute(driver);
-    String firstOpportunity = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/jsl/div[3]/div[7]/div[9]/div/div[1]/div/div/div[5]/div[1]/div[2]/div[2]/h1/span[2]/span[1]"))).getText();
+    if (type.equals("Gyalog")) {
+      return "Gyalog menj";
+    }
+    return "A járatod pedig a " + number + " os " + type;
+  }
+
+  public static String getPublicRoute(WebDriver driver, String type, String number) {
+    WebDriverWait wait = new WebDriverWait(driver, Config.timeOutInSeconds);
+
     SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm");
     Date now = new Date();
     String currentTime = sdfTime.format(now);
-    String[] current = currentTime.split(":");
-    int currentMinutes = Integer.parseInt(current[1]);
-    String[] first = firstOpportunity.split(":");
-    int firstMinutes = Integer.parseInt(first[1]);
-    System.out.println(firstMinutes - currentMinutes);
-    return "yes";
+    //String[] current = currentTime.split(":");
+    //int currentMinutes = Integer.parseInt(current[1]);
+    String fastestRoute;
+    String firstAmount = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/jsl/div[3]/div[7]/div[9]/div/div[1]/div/div/div[3]/div[1]/h1/span[2]/span[3]/span"))).getText();
+    String firstTime = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/jsl/div[3]/div[7]/div[9]/div/div[1]/div/div/div[3]/div[1]/h1/span[2]/span[1]"))).getText();
+    String frequency = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/jsl/div[3]/div[7]/div[9]/div/div[1]/div/div/div[3]/div[2]/div/div[3]/span[3]"))).getText();
+    fastestRoute = "A pontos idő: " + currentTime + ". A leggyorsabb útvonalon " + firstAmount + " alatt érsz oda, és "
+            + firstTime + " kor indul. " + getNumberAndType(driver, type, number) + ". Ezután " + frequency + " jár.";
+    System.out.println(fastestRoute);
+    return fastestRoute;
   }
 }
+/*metró
+/html/body/jsl/div[3]/div[7]/div[9]/div/div[1]/div/div/div[5]/div[1]/div[2]/div[2]/div[2]/span[3]/span[2]/span[1]/span[1]/span
+busz
+/html/body/jsl/div[3]/div[7]/div[9]/div/div[1]/div/div/div[5]/div[1]/div[2]/div[2]/div[2]/span[3]/span[2]/span[1]/span[1]/span*/
