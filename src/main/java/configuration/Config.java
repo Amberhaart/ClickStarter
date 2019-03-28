061d86229c884e5e5c2426b5190c8f10b21efef1
KulusZsu <kulus.zsuzsanna@kodo.hu>
2019-03-28 11:50:25 +0100

    creating Wheather keyword class

--- src/main/java/configuration/Config.java
+++ src/main/java/configuration/Config.java
@@ -1,8 +1,8 @@
 package configuration;
 
 public class Config {
-  public static final String webDriverName = "";
-  public static final String webDriverPath = "";
+  public static final String webDriverName = "webdriver.gecko.driver";
+  public static final String webDriverPath = "C:\\Drivers\\browserdrivers\\geckodriver.exe";
   public static final String userName = "";
   public static final String password = "";
   public static final int timeOutInSeconds = 30;
---
+++ src/main/java/keywords/Weather.java
@@ -0,0 +1,34 @@
+package keywords;
+
+import configuration.Config;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Weather {
+  public static WebDriverWait webDriverWait;
+  public static WebDriver driver;
+  public static String url = "https://www.idokep.hu/elorejelzes/Budapest";
+  public static WebElement dailyForecastLeft;
+  public static WebElement dailyForecastRight;
+
+  public static void goToWeatherPage(WebDriver driver, String url) {
+    webDriverWait = new WebDriverWait(driver, Config.timeOutInSeconds);
+    driver.manage().window().maximize();
+    driver.get(url);
+  }
+
+  public static String getWeatherForecastText(WebDriver driver) {
+    webDriverWait = new WebDriverWait(driver, Config.timeOutInSeconds);
+    goToWeatherPage(driver, url);
+    dailyForecastLeft = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("bal")));
+    dailyForecastRight = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("jobb")));
+    return dailyForecastLeft.getText() + dailyForecastRight.getText();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(getWeatherForecastText(driver));
+  }
+}
