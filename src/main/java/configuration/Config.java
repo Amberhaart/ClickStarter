018a18112f8e258d3d1e587b3294adb48d961b1e
Roland Bak <bakroland0@gmail.com>
2019-03-28 10:20:42 +0100

    init():

---
+++ src/main/java/configuration/Config.java
@@ -0,0 +1,9 @@
+package configuration;
+
+public class Config {
+  public static final String webDriverName = "";
+  public static final String webDriverPath = "";
+  public static final String userName = "";
+  public static final String password = "";
+  public static final int timeOutInSeconds = 30;
+}
