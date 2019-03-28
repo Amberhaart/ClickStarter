e8986f0a9b9de41c278a41969b929773f1b41a1a
KulusZsu <kulus.zsuzsanna@kodo.hu>
2019-03-28 13:01:53 +0100

    fix(Weather): cleancode and styleguide checked

--- src/main/java/keywords/Weather.java
+++ src/main/java/keywords/Weather.java
@@ -3,7 +3,6 @@ package keywords;
 import configuration.Config;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,8 +10,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Weather {
   public static WebDriverWait webDriverWait;
   public static String url = "https://www.idokep.hu/elorejelzes/Budapest";
-  public static WebElement dailyForecastLeft;
-  public static WebElement dailyForecastRight;
 
   public static void goToWeatherPage(WebDriver driver, String url) {
     webDriverWait = new WebDriverWait(driver, Config.timeOutInSeconds);
@@ -22,9 +19,8 @@ public class Weather {
 
   public static String getWeatherForecastText(WebDriver driver) {
     goToWeatherPage(driver, url);
-    dailyForecastLeft = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("bal")));
-    dailyForecastRight = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("jobb")));
-    return dailyForecastLeft.getText() + dailyForecastRight.getText();
+    return webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("bal"))).getText()
+        + webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("jobb"))).getText();
   }
 
   public static void main(String[] args) {
