5546837cb127fa26fbfac8a187a07c3bfb7743d1
KulusZsu <kulus.zsuzsanna@kodo.hu>
2019-03-28 13:39:29 +0100

    feat(Train-Schedule): creating goToTrainSchedulPage method

--- src/main/java/keywords/TrainSchedule.java
+++ src/main/java/keywords/TrainSchedule.java
@@ -1,5 +1,22 @@
 package keywords;
 
+import configuration.Config;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class TrainSchedule {
+  public static WebDriverWait webDriverWait;
+  public static String url = "http://elvira.mav-start.hu/";
+
+  public static void goToTrainSchedulPage(WebDriver driver, String url) {
+    webDriverWait = new WebDriverWait(driver, Config.timeOutInSeconds);
+    driver.manage().window().maximize();
+    driver.get(url);
+  }
+
+  public static void serachFromSchedule(WebDriver driver) {
+
+  }
+
 
 }
