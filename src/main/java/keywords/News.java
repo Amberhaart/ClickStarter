747aa58ae73a72fef06f536390c1d36056358ef5
Noemi Csaszari <csaszari.noemi@gmail.com>
2019-03-28 14:47:42 +0100

    feat(News): add read function

--- src/main/java/keywords/News.java
+++ src/main/java/keywords/News.java
@@ -10,23 +10,54 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class News {
+  private static final String newsUrl = "https://hvg.hu/";
+  private static final String translateUrl = "https://translate.google.com/?hl=hu";
+
   public static WebElement getMainNewsContainer(WebDriver driver) {
+    driver.get(newsUrl);
     WebDriverWait wait = new WebDriverWait(driver, Config.timeOutInSeconds);
     return wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class=\"cimlap-grid-elem elem-375 panelgap\"]")));
   }
 
-  public static String getHeadlines(WebDriver driver) {
+  public static Long getHeadLineCounter(WebDriver driver){
     getMainNewsContainer(driver);
     JavascriptExecutor js = (JavascriptExecutor) driver;
-    String headlines = (String) js.executeScript (" return document.getElementsByClassName(\"cimlap-grid-elem\")[1].textContent");
-    System.out.println(headlines);
+    return (Long) js.executeScript (" return document.getElementsByClassName(\"cimlap-grid-elem\")[1].childElementCount");
+  }
+
+  public static String getHeadline(WebDriver driver, int numberOfHeadline) {
+    getMainNewsContainer(driver);
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    String headlines = (String) js.executeScript (" return document.getElementsByClassName(\"cimlap-grid-elem\")[1].children[" + numberOfHeadline + "].innerText");
     return headlines;
+  }
+
+  public static void readItOutLoud(WebDriver driver, String textToRead){
+    driver.get(translateUrl);
+    WebDriverWait wait = new WebDriverWait(driver, Config.timeOutInSeconds);
+    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//textarea[@id=\"source\"]"))).sendKeys(textToRead);
+    try {
+      Thread.sleep(2000);
+    } catch (Exception e) {
+    }
+    WebElement loudBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div/div/div[5]/div[3]/div[2]")));
+    loudBtn.click();
+  }
+
+
+  public static void readAllNewsOutWithWait(WebDriver driver){
+    for (int i = 0; i < getHeadLineCounter(driver); i++){
+      readItOutLoud(driver, getHeadline(driver, i));
+      try {
+        Thread.sleep(10000);
+      } catch (Exception e) {
+      }
+    }
   }
 
   public static void main(String[] args) {
     System.setProperty(Config.webDriverName, Config.webDriverPath);
     WebDriver driver = new FirefoxDriver();
-    driver.get("https://hvg.hu/");
-    getHeadlines(driver);
+    readAllNewsOutWithWait(driver);
   }
 }
